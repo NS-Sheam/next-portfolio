4646fc8b6636adf5861adca1a072f32108783870
NS-Sheam <123sheamfeni@gmail.com>
2024-01-06 12:13:42 +0600

    no change

--- src/app/(SubLayout)/layout.js
+++ src/app/(SubLayout)/layout.js
@@ -1,9 +1,27 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+
 import { Suspense } from "react";
 
 const layout = ({ children }) => {
   return (
+    // <AnimatePresence mode="wait">
+    //   <motion.div
+    //     key={route}
+    //     initial="pageInitial"
+    //     animate="pageAnimate"
+    //     variants={{
+    //       pageInitial: {
+    //         opacity: 0,
+    //       },
+    //       pageAnimate: {
+    //         opacity: 1,
+    //       },
+    //     }}
+    //     transition={{
+    //       duration: 0.5,
+    //     }}
+    //   >
     <div className={` min-h-screen max-w-screen-xl mx-auto `}>
       <div className="bg-gradient-to-r from-bandPrimary via-bandSecondary to-bandTernary">
         <Header />
@@ -20,6 +38,8 @@ const layout = ({ children }) => {
         <Footer />
       </Suspense>
     </div>
+    //   </motion.div>
+    // </AnimatePresence>
   );
 };
 
