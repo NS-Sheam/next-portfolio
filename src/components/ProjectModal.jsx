ea10ef3dbf8863016cac8bf3066e23a8fbd6a1be
NS-Sheam <123sheamfeni@gmail.com>
2024-01-07 23:54:31 +0600

    modal fixed

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -19,84 +19,81 @@ const ProjectModal = ({ projectData }) => {
   } = projectData;
 
   return (
-    <div>
-      {/* The button to open modal */}
+    <dialog
+      id="my_modal_4"
+      className="fixed z-50 overflow-y-auto h-[calc(100vh-20vh)]"
+    >
 
-      <dialog
-        id="my_modal_4"
-        className="modal w-11/12 max-w-5xl bg-bandSecondary p-4"
-      >
-        <form method="dialog" className="modal-box ">
-          <div className="modal-action">
-            <div className="flex flex-col md:flex-row items-center justify-center gap-6 lg:gap-10 w-full">
-              <div className="md:w-1/2 rounded-lg">
-                <Image
-                  blurDataURL="../assets/images/banner3.jpg"
-                  placeholder="blur"
-                  height={700}
-                  width={700}
-                  layout="responsive"
-                  className="rounded-lg"
-                  src={image}
-                  alt=""
-                />
-              </div>
-              <div className="md:w-1/2 space-y-2">
-                <h1 className="text-xl lg:text-3xl text-bandYellow font-bold text-center">
-                  {projectName}
-                </h1>
-                <h3 className="text-base lg:text-2xl font-bold text-bandYellow">
-                  Description:{" "}
-                </h3>
-                <ul className="text-white text-sm lg:text-base list-disc">
-                  {description?.map((d, i) => (
-                    <li key={i}>{d}</li>
-                  ))}
-                </ul>
-                <h3 className="text-xl lg:text-2xl font-bold text-bandYellow">
-                  Technologies:{" "}
-                </h3>
-                <ul className="text-white text-base list-disc z-10">
-                  {technology?.map((tec, i) => (
-                    <li key={i}>{tec}</li>
-                  ))}
-                </ul>
+      <div className="modal-action p-4 lg:p-8">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-6 lg:gap-10 w-full">
+          <div className="md:w-1/2 rounded-lg">
+            <Image
+              blurDataURL="../assets/images/banner3.jpg"
+              placeholder="blur"
+              height={700}
+              width={700}
+              layout=""
+              className="rounded-lg"
+              src={image}
+              alt=""
+            />
+          </div>
+          <div className="md:w-1/2 space-y-2">
+            <h1 className="text-xl lg:text-3xl text-bandYellow font-bold text-center">
+              {projectName}
+            </h1>
+            <h3 className="text-base lg:text-2xl font-bold text-bandYellow">
+              Description:{" "}
+            </h3>
+            <ul className="text-white text-sm lg:text-base list-disc">
+              {description?.map((d, i) => (
+                <li key={i}>{d}</li>
+              ))}
+            </ul>
+            <h3 className="text-xl lg:text-2xl font-bold text-bandYellow">
+              Technologies:{" "}
+            </h3>
+            <ul className="text-white text-base list-disc z-10">
+              {technology?.map((tec, i) => (
+                <li key={i}>{tec}</li>
+              ))}
+            </ul>
 
-                <div className="social-buttons flex items-center justify-start gap-6 z-20">
-                  <a
-                    href={liveSite}
-                    className="social-button social-button--instagram "
-                    aria-label="Instagram"
-                  >
-                    <RxOpenInNewWindow />
-                  </a>
-                  <a
-                    href={clientSite}
-                    className="social-button social-button--github"
-                    aria-label="GitHub"
-                  >
-                    <FaGithub />
-                  </a>
+            <div className="social-buttons flex items-center justify-start gap-6 z-20">
+              <a
+                href={liveSite}
+                className="social-button social-button--instagram "
+                aria-label="Instagram"
+              >
+                <RxOpenInNewWindow />
+              </a>
+              <a
+                href={clientSite}
+                className="social-button social-button--github"
+                aria-label="GitHub"
+              >
+                <FaGithub />
+              </a>
 
-                  {serverSite && (
-                    <a
-                      href={serverSite}
-                      className="social-button social-button--hire-me"
-                      aria-label="Server Site"
-                    >
-                      <FaLink />
-                    </a>
-                  )}
-                </div>
-              </div>
+              {serverSite && (
+                <a
+                  href={serverSite}
+                  className="social-button social-button--hire-me"
+                  aria-label="Server Site"
+                >
+                  <FaLink />
+                </a>
+              )}
             </div>
           </div>
-          <button className="btn text-white bg-pink-600 hover:scale-110 duration-200 font-extrabold btn-sm btn-circle btn-ghost absolute right-2 top-2  hover:bg-warning border-none rounded-full w-8 h-8">
-            ✕
-          </button>
-        </form>
-      </dialog>
-    </div>
+        </div>
+      </div>
+      <button className="btn text-white bg-pink-600 hover:scale-110 duration-200 font-extrabold btn-sm btn-circle btn-ghost absolute right-2 top-2  hover:bg-warning border-none rounded-full w-8 h-8">
+        ✕
+      </button>
+
+    </dialog>
+
   );
 };
 
