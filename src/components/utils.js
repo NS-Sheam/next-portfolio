85b5a8cbe4c0ba41f405bb831f0894454869f9d4
NS-Sheam <123sheamfeni@gmail.com>
2024-01-08 17:54:41 +0600

    no change

--- src/components/utils.js
+++ src/components/utils.js
@@ -22,9 +22,9 @@ export const generateMetaDatas = async (
   type,
 ) => {
   return {
-    title: title ? "Nazmus Sakib|" + title : "Nazmus Sakib",
+    title: title ? "Nazmus Sakib | " + title : "Nazmus Sakib",
     description: description || "Welcome to the sky of Nazmus Sakib",
-    image: image.src,
+    image: image?.src || myimg.src,
     url: path ? "https://nazmus-sakib.me/" + path : "https://nazmus-sakib.me/",
     type: type ? "Nazmus Sakib" + type : "Nazmus Sakib",
     siteName: "Nazmus Sakib",
@@ -39,7 +39,7 @@ export const generateMetaDatas = async (
       description: description || "Welcome to the sky of Nazmus Sakib",
       images: [
         {
-          url: image.src,
+          url: image?.src,
         },
       ],
       site_name: "Nazmus Sakib",
