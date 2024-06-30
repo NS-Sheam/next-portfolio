99209f5ea7aa2af1b07908b929c72b43a55110aa
NS-Sheam <123sheamfeni@gmail.com>
2024-06-30 23:04:43 +0600

    refactor: Remove commented code and unused imports in HomeBlogs.jsx

--- src/components/Home/HomeBlogs/HomeBlogs.jsx
+++ src/components/Home/HomeBlogs/HomeBlogs.jsx
@@ -1,12 +1,12 @@
 
 import SectionTitle from "../../../components/SectionTitle";
 import BlogCard from "../../../components/BlogCard";
-import { BlogFunction } from "@/components/utils/blogFn";
-import BlogsLoader from "@/components/BlogsLoader";
+// import { BlogFunction } from "@/components/utils/blogFn";
+// import BlogsLoader from "@/components/BlogsLoader";
 
 const HomeBlogs = async () => {
   // const { blogs, loading } = await BlogFunction.allBlogs();
-  const { loading } = await BlogFunction.allBlogs();
+  // const { loading } = await BlogFunction.allBlogs();
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/blogs`,
@@ -23,12 +23,12 @@ const HomeBlogs = async () => {
       <div className="container mx-auto inner-container space-y-4">
         <SectionTitle heading="Blogs" punchLine="Read my blogs" />
         {
-          loading ? <BlogsLoader /> :
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-end mx-auto my-5 gap-5 -mt-8 ">
-              {/* {blogs?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)} */}
-              {serverBlogs?.data?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)}
+          // loading ? <BlogsLoader /> :
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-end mx-auto my-5 gap-5 -mt-8 ">
+            {/* {blogs?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)} */}
+            {serverBlogs?.data?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)}
 
-            </div>
+          </div>
         }
       </div>
     </section>
