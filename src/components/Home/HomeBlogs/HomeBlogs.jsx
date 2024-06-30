911316ab170327dc9e7011eb12a3c3ff18cc4a52
NS-Sheam <123sheamfeni@gmail.com>
2024-07-01 00:09:08 +0600

    refactor: Update HomeBlogs.jsx

--- src/components/Home/HomeBlogs/HomeBlogs.jsx
+++ src/components/Home/HomeBlogs/HomeBlogs.jsx
@@ -10,7 +10,7 @@ const HomeBlogs = async () => {
   // const { blogs, loading } = await BlogFunction.allBlogs();
   // const { loading } = await BlogFunction.allBlogs();
 
-  const { blogs, loading } = await BlogFunction.allBlogsFromServer();
+  const { loading } = await BlogFunction.allBlogsFromServer();
 
 
 
@@ -22,16 +22,17 @@ const HomeBlogs = async () => {
       <div className="container mx-auto inner-container space-y-4">
         <SectionTitle heading="Blogs" punchLine="Read my blogs" />
         {
-          loading ? <BlogsLoader /> :
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-end mx-auto my-5 gap-5 -mt-8 ">
-              {blogs?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)}
+          loading && <div>dfd</div>
+          // loading ? <BlogsLoader /> :
+          //   <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-end mx-auto my-5 gap-5 -mt-8 ">
+          //     {blogs?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)}
               {/* {
             serverBlogs ?
               serverBlogs?.data?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />) :
               <BlogsLoader />
           } */}
 
-            </div>
+        </div>
         }
       </div>
     </section>
