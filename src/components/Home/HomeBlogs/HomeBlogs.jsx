d752a10f9789b169a45b5d7b9076da4f76bc4bd8
NS-Sheam <123sheamfeni@gmail.com>
2024-06-30 23:59:00 +0600

    refactor: Update

--- src/components/Home/HomeBlogs/HomeBlogs.jsx
+++ src/components/Home/HomeBlogs/HomeBlogs.jsx
@@ -9,46 +9,42 @@ const HomeBlogs = async () => {
   // const { blogs, loading } = await BlogFunction.allBlogs();
   // const { loading } = await BlogFunction.allBlogs();
 
-  try {
-    const res = await fetch(
-      `${process?.env?.NEXT_PUBLIC_SERVER_URL}/blogs`,
-      {
-        cache: "no-cache",
-      }
-
-    );
-    if (!res.ok) {
-      throw new Error(`Failed to fetch blogs: ${res.status} ${res.statusText}`);
+  const res = await fetch(
+    `${process?.env?.NEXT_PUBLIC_SERVER_URL}/blogs`,
+    {
+      cache: "no-cache",
     }
-    const serverBlogs = await res?.json() || {};
-    console.log(serverBlogs);
 
-
-    return (
-      <section className="bg-gradient-to-tr bg-bandTernary  py-8 lg:p-14">
-        <div className="container mx-auto inner-container space-y-4">
-          <SectionTitle heading="Blogs" punchLine="Read my blogs" />
-          {
-            // loading ? <BlogsLoader /> :
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-end mx-auto my-5 gap-5 -mt-8 ">
-              {/* {blogs?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)} */}
-              {
-                serverBlogs ?
-                  serverBlogs?.data?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />) :
-                  <BlogsLoader />
-              }
-
-            </div>
-          }
-        </div>
-      </section>
-    );
-  } catch (error) {
-    console.error("error response", error);
+  );
+  if (!res) {
     return {
       notFound: true,
-    };
+    }
+
   }
+  const serverBlogs = await res?.json() || {};
+  console.log(serverBlogs);
+
+
+  return (
+    <section className="bg-gradient-to-tr bg-bandTernary  py-8 lg:p-14">
+      <div className="container mx-auto inner-container space-y-4">
+        <SectionTitle heading="Blogs" punchLine="Read my blogs" />
+        {
+          // loading ? <BlogsLoader /> :
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-end mx-auto my-5 gap-5 -mt-8 ">
+            {/* {blogs?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />)} */}
+            {
+              serverBlogs ?
+                serverBlogs?.data?.slice(0, 3).map(blog => <BlogCard key={blog._id} blog={blog} />) :
+                <BlogsLoader />
+            }
+
+          </div>
+        }
+      </div>
+    </section>
+  );
 };
 
 export default HomeBlogs;
